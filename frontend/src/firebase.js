50e10bc1a90d26883f171811f0b81a457b43a040
Kaustav Halder <kaustavhalder2706@gmail.com>
2026-03-10 19:43:59 +0530

    Configured Firebase auth and firestore

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCdfmTy_-M-0jTHy3zRLEZYUrJcVjCiaLo",
+  apiKey: "AIzaSyCdfmTy_-M-0jThy3zRLEZYUrJcVjCiaLo",
   authDomain: "iit-bhu-healthcare.firebaseapp.com",
   projectId: "iit-bhu-healthcare",
   storageBucket: "iit-bhu-healthcare.firebasestorage.app",
   messagingSenderId: "778672094077",
-  appId: "1:778672094077:web:78eeb0826b97f955272a14",
-  measurementId: "G-WEV7QN0ZGM"
+  appId: "1:778672094077:web:78eeb0826b97f955272a14"
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
 export const db = getFirestore(app);
